8722f3e80d2cf0d2c7738c1e8682f4350ed957c5
Catleader1407 <tringuyen14071992@gmail.com>
2026-03-29 21:11:58 +0700

    fix: copy all 13 remote bundles to remote/ directory
    Engine constructs {server}/remote/{bundleName}/ for remoteBundles.
    Original bundles were at CDN root only. Now available at both paths.

---
+++ remote/spine/index.22341.js
@@ -0,0 +1,20 @@
+System.register("chunks:///spine.js",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/spine', 'chunks:///spine.js'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
